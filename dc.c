4f20f4516a001834f55f50ac76a28a8dc6d43d5f
javieratorresm <jakane.x@hotmail.com>
2016-11-27 14:14:51 -0300

    segundo programa

--- dc.c
+++ dc.c
@@ -7,12 +7,14 @@ int main()
 {
 int *elementos;
 elementos = (int*)malloc(sizeof(int)*536870912);
+int random=0;
 int i; /*declaración de los contadores*/
 clock_t start = clock();
 
 for (i=0;i<536870912;i++){
-    elementos[i]= rand();
-    //printf("%i %i \n",i,elementos[i]);
+	random=rand()%10000000;
+    elementos[random]= i;
+    //printf("%i %i \n",i,random);
 }
 printf("%f\n", ((double)clock()-start )/ CLOCKS_PER_SEC);
 	free(elementos);
